add449f865451f62ac518e8f1059c2e1bf7a3afa
H4WG <shyter2012@gmail.com>
2020-12-04 03:15:55 +0200

    Додавання лабораторної роботи #8

---
+++ Лабораторна робота #8/1.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>example_2</title>
+        <meta charset="UTF-8">
+    </head>
+    <body>
+
+        <form action="2.php" method="get">
+            <p>Ввести значення змінних:</p>
+            a: <input name="a" type="text" value=""><br>
+            b: <input name="b" type="text" value=""> <br>
+            <input type="submit" value="Розрахувати">
+        </form>
+
+    </body>
+</html>
